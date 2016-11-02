ccdab5d89a021566932fc3cf797ec5b574b0e04e
wangmattychen <chenmatty.wang@gmail.com>
2016-11-02 17:04:56 -0400

    finished exercise 1

--- script/app.js
+++ script/app.js
@@ -1,1 +1,12 @@
 /* Write your code here! */
+$(".announcement").append('<button type="button" class="closeBtn">X</button>');
+
+$(".closeBtn").click(function() {
+	var announcement = $(this).parent('.announcement');
+	announcement.toggleClass('hide');
+	if (announcement.hasClass('hide')) {
+		$(this).html("show announcement");
+	} else {
+		$(this).html("X");
+	}
+});
