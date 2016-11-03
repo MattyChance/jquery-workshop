07b02a9a06694adc7db9e897a1e8221b4060a2f8
wangmattychen <chenmatty.wang@gmail.com>
2016-11-02 21:29:26 -0400

    added exercise 2

--- script/app.js
+++ script/app.js
@@ -1,1 +1,20 @@
 /* Write your code here! */
+// $('li').click(function () {
+
+// 	$(this).parent().parent().toggleClass('overlay');
+// })
+
+$('img').click(function(){
+	var imgAlt = $(this).attr('alt');
+	var imgSrc = $(this).attr('src');
+	console.log(imgSrc);
+	console.log(imgAlt);
+	$('.overlay img').attr('src', imgSrc);
+	$('.image-caption').text(imgAlt);
+	$('.overlay').addClass('show');
+	$('.overlay').click(function() {
+		$(this).removeClass('show');
+	});
+
+});
+
